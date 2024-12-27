7d5155a8506ebd9bf575de30a0ea37bc7fc54efd
mohammed-nuh <mohammednuhabuhuseina@gmail.com>
2024-12-27 18:25:03 +0530

    function files of ts and js

---
+++ functions.js
@@ -0,0 +1,4 @@
+function addNums(num1, num2) {
+    return num1 + num2;
+}
+console.log("Sum = ".concat(addNums(5, 6)));
---
+++ functions.ts
@@ -0,0 +1,5 @@
+function addNums(num1: number, num2: number): number {
+    return num1 + num2;
+}
+
+console.log(`Sum = ${addNums(5,6)}`);
